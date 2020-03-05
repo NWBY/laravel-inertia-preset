5d52706f6412daee0baaa750b46cd4ecdf2637de
NWBY <sam.newby19@gmail.com>
2020-03-05 20:18:21 +0000

    Major progress

--- src/LaravelInertiaServiceProvider.php
+++ src/LaravelInertiaServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Nwby\LaravelInertia;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
--- src/Preset.php
+++ src/Preset.php
@@ -1,10 +1,46 @@
 <?php
 
+namespace Nwby\LaravelInertia;
+
 use Illuminate\Foundation\Console\Presets\Preset as LaravelPreset;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
 
 class Preset extends LaravelPreset
 {
     public static function install()
     {
+        static::cleanAssets();
+        static::updatePackages();
+        static::updateMix();
+        static::updateJs();
+    }
+
+    public static function cleanAssets()
+    {
+        File::cleanDirectory(resource_path('assets/js'));
+        File::cleanDirectory(resource_path('assets/sass'));
+    }
+
+    public static function updatePackageArray($packages)
+    {
+        return array_merge([
+            'tailwindcss' => 'latest',
+            '@inertiajs/inertia' => 'latest',
+            '@inertiajs/inertia-vue' => 'latest'
+        ], Arr::except($packages, [
+            'popper.js',
+            'lodash'
+        ]));
+    }
+
+    public static function updateMix()
+    {
+        copy(__DIR__ . '/stubs/webpack.mix.js', base_path('webpack.mix.js'));
+    }
+
+    public static function updateJs()
+    {
+        copy(__DIR__ . '/stubs/app.js', resource_path('assets/js/app.js'));
     }
 }
